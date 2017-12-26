34bfb41db0205247d9fcfa0872078902fc8c254d
Taggert <4uxpux@mail.ru>
2017-12-26 22:41:48 +0200

    Removed small mistakes

--- src/com/company/TestProcessing.java
+++ src/com/company/TestProcessing.java
@@ -23,23 +23,36 @@ public class TestProcessing {
         res.put(TestResults.FAILED, new ArrayList<>());
         res.put(TestResults.SKIPPED, new ArrayList<>());
 
+        //Extracting methods
         Method[] methods = c.getDeclaredMethods();
-
+        // Setting up preparable methods
         Method before = null;
         Method setUp = null;
         Method after = null;
         Method destroy = null;
         for (Method m : methods) {
             if (m.isAnnotationPresent(SetUp.class)) {
+                if (!(setUp == null)) {
+                    throw new RuntimeException("SetUp used more than one time");
+                }
                 setUp = m;
             }
             if (m.isAnnotationPresent(Before.class)) {
+                if (!(before == null)) {
+                    throw new RuntimeException("Before used more than one time");
+                }
                 before = m;
             }
             if (m.isAnnotationPresent(After.class)) {
+                if (!(after == null)) {
+                    throw new RuntimeException("After used more than one time");
+                }
                 after = m;
             }
             if (m.isAnnotationPresent(Destroy.class)) {
+                if (!(destroy == null)) {
+                    throw new RuntimeException("Destroy used more than one time");
+                }
                 destroy = m;
             }
         }
@@ -49,25 +62,32 @@ public class TestProcessing {
             setUp.invoke(tc);
         }
         for (Method m : methods) {
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).isEnabled()) {
-                if (before != null) before.invoke(tc);
-                try {
-                    m.invoke(tc);
-                    res.get(res.get(TestResults.PASSED)).add(m.getName());
-                } catch (InvocationTargetException e) {
-                    Throwable cause = e.getCause();
-                    if (m.isAnnotationPresent(Expected.class) && m.getAnnotation(Expected.class).c().equals(cause.getClass())) {
-                        res.get(res.get(TestResults.PASSED)).add(m.getName());
-                    } else {
-                        res.get(res.get(TestResults.FAILED)).add(m.getName());
-                    }
-                }
-                if (after != null) after.invoke(tc);
-            } else if (m.isAnnotationPresent(Test.class) && !m.getAnnotation(Test.class).isEnabled()) {
-                if (before != null) before.invoke(tc);
-                res.get(res.get(TestResults.SKIPPED)).add(m.getName());
-                if (after != null) after.invoke(tc);
+            if (!m.isAnnotationPresent(Test.class)) {
+                continue;
             }
+            if (!m.getAnnotation(Test.class).isEnabled()) {
+                res.get(TestResults.SKIPPED).add(m.getName());
+                continue;
+            }
+
+            //Peforming test
+            TestResults result = TestResults.PASSED;
+            if (before != null) {
+                before.invoke(tc);
+            }
+            try {
+                m.invoke(tc);
+            } catch (InvocationTargetException e) {
+                Throwable cause = e.getCause();
+                if (!m.isAnnotationPresent(Expected.class) ||  !m.getAnnotation(Expected.class).c().equals(cause.getClass())) {
+                    result = TestResults.FAILED;
+                }
+            }
+            res.get(result).add(m.getName());
+            if (after != null) {
+                after.invoke(tc);
+            }
+
         }
 
         if (destroy != null) destroy.invoke(tc);
